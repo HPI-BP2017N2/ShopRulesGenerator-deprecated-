8127d93b8682fa6fab7074c16a84b43f83c44ff9
deeps96 <leonardo.huebscher.business@gmail.com>
2018-01-25 14:48:13 +0100

    Created AttributeEntry - data object. #6

--- src/main/java/de/hpi/shoprulesgenerator/model/AttributeEntry.java
+++ src/main/java/de/hpi/shoprulesgenerator/model/AttributeEntry.java
@@ -1,4 +1,18 @@
 package de.hpi.shoprulesgenerator.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashMap;
+
+@Getter @Setter
 public class AttributeEntry {
+
+    private HashMap<String, Integer> selectorCountMap;
+    private int attributeValueFound;
+
+    private AttributeEntry() {
+        setAttributeValueFound(0);
+        setSelectorCountMap(new HashMap<>());
+    }
 }
